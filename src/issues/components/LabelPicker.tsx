ca3ec8fa8e088cde6c1c07c7a98ab702ee863b9d
Ramiro Pacheco <ramiro@bankaya.com.mx>
2023-06-29 13:05:21 -0700

    Complete to add process of select labels.

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,10 +1,17 @@
+import { FC } from 'react';
 import { useLabels } from '../hooks/useLabels';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabel: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({ selectedLabel, onChange }) => {
   const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading) {
-    return(<h1>loading...</h1>)
+    return(<LoadingIcon />)
   }
 
   return (
@@ -14,8 +21,11 @@ export const LabelPicker = () => {
 
           <span
             key={label.id}
-            className="badge rounded-pill m-1 label-picker"
+            className={`
+              badge rounded-pill m-1 label-picker ${selectedLabel.includes(label.name) ? 'label-active' : ''}
+            `}
             style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+            onClick={() => onChange(label.name)}
           >
             { label.name }
           </span>
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -14,7 +14,30 @@ const getLabels = async(): Promise<Labels[]> => {
 export const useLabels = () => {
   const labelsQuery = useQuery(
     ['labels'], // nombre de espacio en caché
-    getLabels
+    getLabels,
+    {
+      staleTime: 1000 * 60 * 60,
+      // placeholderData: [],
+      // initialData: []
+      placeholderData: [
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken) 2",
+          color: "b60205",
+          default: false,
+        },
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue 2",
+          color: "6ce26a",
+          default: true,
+        }
+      ]
+    }
   );
 
   return {
